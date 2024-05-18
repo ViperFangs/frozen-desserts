1ecb7a0415f95744b31a8a1db5f03f2edbbe8a0c
ViperFangs <amathrej@asu.edu>
2024-05-18 00:04:43 -0700

    Create AWS RDS Database in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,35 @@
 import pulumi
 import pulumi_aws as aws
+import base64
+
+config = pulumi.Config()
+db_username = config.require("dbUsername")
+db_password = config.require("dbPassword")
 
 # Read the user_data script
 with open('user_data.sh', 'r') as user_data_file:
     user_data_script = user_data_file.read()
+
+# Create a security group for RDS
+rds_security_group = aws.ec2.SecurityGroup('frozen-desserts-rds-sg',
+    description='Allow database traffic',
+    ingress=[
+        {
+            'protocol': 'tcp',
+            'from_port': 5432,  # Accept Database Messages on the default port of Postgres
+            'to_port': 5432,
+            'cidr_blocks': ['0.0.0.0/0'],
+        },
+    ],
+    egress=[
+        {
+            'protocol': '-1',
+            'from_port': 0,
+            'to_port': 0,
+            'cidr_blocks': ['0.0.0.0/0'],
+        },
+    ]
+)
 
 # Create a security group
 security_group = aws.ec2.SecurityGroup('frozen-desserts-sg',
@@ -47,8 +73,29 @@ security_group = aws.ec2.SecurityGroup('frozen-desserts-sg',
 # Create an S3 bucket
 bucket = aws.s3.Bucket('frozen-desserts-bucket')
 
+# Create an RDS instance
+rds_instance = aws.rds.Instance('frozen-desserts-db',
+    allocated_storage=10,
+    engine='postgres',
+    instance_class='db.t3.micro',
+    db_name='frozenDessertsDB',
+    username=db_username,
+    password=db_password,
+    vpc_security_group_ids=[rds_security_group.id],
+    deletion_protection=True,
+    publicly_accessible=True,
+    skip_final_snapshot=True
+)
+
+user_data = pulumi.Output.all(db_username, db_password, rds_instance.endpoint).apply(
+    lambda args: user_data_script
+                  .replace("${DB_USERNAME}", args[0])
+                  .replace("${DB_PASSWORD}", args[1])
+                  .replace("${DB_HOST}", args[2])
+)
+
 # Get the latest Amazon Linux 2 AMI
-ami = aws.ec2.get_ami(most_recent=True,
+ami = aws.ec2.get_ami(most_recent=True, 
                       owners=["amazon"],
                       filters=[{"name": "name", "values": ["amzn2-ami-hvm-*"]}])
 
@@ -57,11 +104,23 @@ instance = aws.ec2.Instance('frozen-desserts-instance',
                             instance_type='t2.micro',
                             ami=ami.id,
                             security_groups=[security_group.name],
-                            user_data=user_data_script,
+                            user_data=user_data.apply(lambda script: base64.b64encode(script.encode('utf-8')).decode('utf-8')),
                             tags={"Name": "frozen-desserts-server"})
+
+# Create an Elastic IP
+elastic_ip = aws.ec2.Eip('frozen-desserts-eip', 
+                         opts=pulumi.ResourceOptions(ignore_changes=["instance"]))
+
+# Associate the Elastic IP with the EC2 instance
+eip_association = aws.ec2.EipAssociation('frozen-desserts-eip-association',
+                                         instance_id=instance.id,
+                                         allocation_id=elastic_ip.id)
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
 
-# Export the instance's public IP
-pulumi.export('public_ip', instance.public_ip)
+# Export the Elastic IP
+pulumi.export('elastic_ip', elastic_ip.public_ip)
+
+# Export the Elastic IP
+pulumi.export('rds_endpoint', rds_instance.endpoint)
